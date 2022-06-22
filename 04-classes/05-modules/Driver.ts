ead603e1f52dc35bb4c9d0b65ed4d762781047c5
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-06-22 08:46:14 -0700

    classes

---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer} from "./Customer";
+
+// let's create an instance
+let myCustomer = new Customer("Steven", "Liu");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
